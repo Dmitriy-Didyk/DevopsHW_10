d77751502d62d11e1856311f3073837b91d5bf7f
Dmitriy-Didyk <didyk.dmitriy@apriorit.com>
2024-11-19 14:01:44 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,8 +2,8 @@ provider "azurerm" {
   features {}
 
   # ID подписки, клиента, секрет и tenant для аутентификации
-  subscription_id = ""
-  client_id       = ""
-  client_secret   = ""
-  tenant_id       = ""
+  subscription_id = "14837371-039f-4e1d-8ae8-af5e733ade86"
+  client_id       = "d77e620a-2d05-4e5d-aaeb-ae1c5302a70a"
+  client_secret   = "oY88Q~EwjL1~2wgKUTt0QumeATIKK3WaiOY_Fal9"
+  tenant_id       = "fed13a2f-5620-4dae-85d0-9f757ae5e262"
 }
