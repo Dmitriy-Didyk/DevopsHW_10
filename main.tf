d8602e87fea4470e647a8c59dd8df0c854c41007
Dmitriy-Didyk <didyk.dmitriy@apriorit.com>
2024-11-20 17:07:55 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -139,3 +139,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
   }
 }
 
+resource "azurerm_subnet_network_security_group_association" "subnet_nsg_association" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
