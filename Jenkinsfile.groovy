1282b808a7c43bf24a104b99517d8d47bfba9a4c
Dmitriy-Didyk <didyk.dmitriy@apriorit.com>
2024-11-26 18:48:25 +0200

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,6 +16,18 @@ pipeline {
                 '''
             }
         }
+        stage('Generate Ansible Inventory') {
+            steps {
+                script {
+                    def publicIps = sh(script: "terraform output -json | jq -r '.vm_public_ips.value[]'", returnStdout: true).trim()
+                    writeFile file: 'hosts', text: """
+[azure]
+${publicIps.split('\n').collect { "${it} ansible_user=azureadmin ansible_ssh_private_key_file=/path/to/private/key" }.join('\n')}
+"""
+                }
+                sh 'cat hosts' // Вывод файла hosts для проверки
+            }
+        }
         stage('Configure') {
             steps {
                 echo 'Configuring environment using Ansible...'
