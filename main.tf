49bae4a28452136be919a16afbffb1f6c2011792
Dmitriy-Didyk <didyk.dmitriy@apriorit.com>
2024-11-26 18:32:49 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ resource "azurerm_public_ip" "vm_public_ip" {
   name                = "${var.resource_prefix}-vm-public-ip-${count.index}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
   sku                 = "Standard"
 }
 
